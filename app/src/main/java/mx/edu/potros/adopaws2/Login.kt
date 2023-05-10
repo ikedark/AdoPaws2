22a200d84c138db651a2eff79e4f72df4196ee88
ikedark <david.v1795@gmail.com>
2023-05-10 15:56:21 -0700

    half work

---
+++ app/src/main/java/mx/edu/potros/adopaws2/Login.kt
@@ -0,0 +1,11 @@
+    package mx.edu.potros.adopaws2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login)
+    }
+}
